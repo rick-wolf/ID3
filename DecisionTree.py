6a57b2cd467f9b3291374c071c93f99b68472674
Rick-Wolf <wolf.rick.c@gmail.com>
2015-09-28 14:04:39 -0500

    tree building seems to work correctly

--- DecisionTree.py
+++ DecisionTree.py
@@ -34,27 +34,19 @@ class DecisionTree(object):
 		# get a tuple with the name of the best attribute and its info gain,
 		#    and the split if the feature is numeric
 		bestGain = self.bestAttribAndGain(examples, attributes)
-		print bestGain
-
 		# test if this should be a leaf node
 		#   are the remaining training instances homogeneous?
+		if len(examples) == 0:
+			return DecTreeNode(parentAttrib, parentAttribValue, True, [], 
+				self.labels[0])
 		if (self.isHomogeneousSet(examples)):
-			print "here"
 			return DecTreeNode(parentAttrib, parentAttribValue, True, [], examples[0][-1])
 		#   are there fewer than m instances? OR
 		#   do all remaining features have negative information gain?
 		if len(examples) < m:
-			print "here2"
-			print(m)
-			print(len(examples))
-			print(len(examples)>m)
-			print(bestGain[1])
 			return DecTreeNode(parentAttrib, parentAttribValue, True, [], 
 				self.getMajorityLabel(examples))
-		if  (bestGain[1] < 0):
-			print "here3"
-			print(len(examples))
-			print(bestGain[1])
+		if  (bestGain[1] <= 0):
 			return DecTreeNode(parentAttrib, parentAttribValue, True, [], 
 				self.getMajorityLabel(examples))
 		#   are there only numeric features with no candidate splits?
@@ -65,6 +57,8 @@ class DecisionTree(object):
 		subAttribs = list(attributes) # copy the list
 		xInd = self.attributes.index(bestGain[0])
 		subtree = DecTreeNode(parentAttrib, parentAttribValue, False, [])
+		print bestGain
+		print len(examples)
 		# remove the current attrib from the list of attribs if its nominal
 		if len(bestGain) == 2:
 			subAttribs.remove(bestGain[0])
@@ -84,6 +78,10 @@ class DecisionTree(object):
 		return subtree
 
 
+#	def printNode(self, depth, node):
+#		line = ''
+#		line
+
 
 
 
@@ -99,16 +97,18 @@ class DecisionTree(object):
 		candidates = []
 
 		# get a sorted list of unique values in the attribute
-		uniqueVals = sorted(list(set(zip(*instances)[x])))
-		
-		# get a dict of instances sorted 
-		valSets = {key:[] for key in uniqueVals}
-		for instance in instances:
-			valSets[instance[x]].append((instance[x],instance[-1]))
+		uniqueVals = []
+		if len(instances) > 0:
+			uniqueVals = sorted(list(set(zip(*instances)[x])))
+
+			# get a dict of instances sorted 
+			valSets = {key:[] for key in uniqueVals}
+			for instance in instances:
+				valSets[instance[x]].append((instance[x],instance[-1]))
 		
 		# determine if midpoints between set values should 
 		# be considered for splits
-		if (len(uniqueVals) > 0):
+		if (len(uniqueVals) > 1):
 			for i in range(len(uniqueVals)-1):
 				sLabels = set(zip(*valSets[uniqueVals[i]])[1])
 				tLabels = set(zip(*valSets[uniqueVals[i+1]])[1])
@@ -153,7 +153,6 @@ class DecisionTree(object):
 				if candSplitGain[1] > best[1]:
 					best = (attribute, candSplitGain[1], candSplitGain[0])
 
-		print(best)
 		return best
 
 
@@ -173,12 +172,20 @@ class DecisionTree(object):
 		"""
 		# find instances that are less than or equal to the split
 		x = self.attributes.index(attribute)
-		lteInstances = [instance for instance in instances if instances[x] <= split]
-		gtInstances = [instance for instance in instances if instances[x] > split]
+		#print(len(instances[0]))
+		#print("x is: " + str(x))
+		#print("split is: " +str(split))
+		#print instances
+		lteInstances = [instance for instance in instances if instance[x] <= split]
+		gtInstances = [instance for instance in instances if instance[x] > split]
+		
+		#for instance in lteInstances:
+		#	print instance
+
 
 		# calculate conditional entropy
-		return (((len(lteInstances)/len(instances))*self.getEntropy(lteInstances)) + \
-		((len(gtInstances)/len(instances))*self.getEntropy(gtInstances)))
+		return (((float(len(lteInstances))/len(instances))*self.getEntropy(lteInstances)) + \
+		((float(len(gtInstances))/len(instances))*self.getEntropy(gtInstances)))
 
 
 
@@ -190,10 +197,10 @@ class DecisionTree(object):
 		valList = self.attributeValues[attribute]
 
 		entSum = 0
-
-		for val in valList:
-			valExamples = [instance for instance in instances if instance[xInd] == val]
-			entSum += (float(len(valExamples))/len(instances)) * self.getEntropy(valExamples)
+		if len(instances) > 0:
+			for val in valList:
+				valExamples = [instance for instance in instances if instance[xInd] == val]
+				entSum += (float(len(valExamples))/len(instances)) * self.getEntropy(valExamples)
 
 		return entSum
 
